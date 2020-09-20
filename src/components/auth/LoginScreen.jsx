68d5386d83702195354d6c3e6810dac4fb9a7863
pacomonle <franciscomonleonleal@gmail.com>
2020-09-20 21:11:42 +0200

    conexion backend frontend loginy register redux

--- src/components/auth/LoginScreen.jsx
+++ src/components/auth/LoginScreen.jsx
@@ -1,18 +1,61 @@
 import React from 'react'
+import { useDispatch } from 'react-redux';
+import { useForm } from '../../hooks/useForm.js'
+import { startLogin, startRegister } from '../../actions/auth';
+import Swal from 'sweetalert2';
 import './login.css'
 
 const LoginScreen = () => {
+    
+    const dispatch = useDispatch();
+
+    
+    const [ formLoginValues, handleLoginInputChange ] = useForm({
+        lEmail: 'fernando@gmail.com',
+        lPassword: '123456'
+    });
+
+    const [ formRegisterValues, handleRegisterInputChange ] = useForm({
+        rName: 'Nando',
+        rEmail: 'nando@gmail.com',
+        rPassword1: '123456',
+        rPassword2: '123456'
+    });
+    
+    const { lEmail, lPassword } = formLoginValues;
+    const { rName, rEmail, rPassword1, rPassword2 } = formRegisterValues;
+
+    const handleLogin = ( e ) => {
+        e.preventDefault();
+        dispatch( startLogin( lEmail, lPassword ) );
+    }
+
+    const handleRegister = ( e ) => {
+        e.preventDefault();
+
+        if ( rPassword1 !== rPassword2 ) {
+            return Swal.fire('Error', 'Las contraseñas deben de ser iguales','error');
+        }
+        console.log('?')
+        dispatch( startRegister( rEmail, rPassword1, rName ) );
+    }
+
+
+
     return (
         <div className="container login-container">
         <div className="row">
             <div className="col-md-6 login-form-1">
                 <h3>Ingreso</h3>
-                <form>
+                <form onSubmit={handleLogin}>
                     <div className="form-group">
                         <input 
                             type="text"
                             className="form-control"
                             placeholder="Correo"
+                            name="lEmail"
+                            value={lEmail}
+                            onChange={handleLoginInputChange}
                         />
                     </div>
                     <div className="form-group">
@@ -20,6 +63,9 @@ const LoginScreen = () => {
                             type="password"
                             className="form-control"
                             placeholder="Contraseña"
+                            name= "lPasword"
+                            value={lPassword}
+                            onChange={handleLoginInputChange}
                         />
                     </div>
                     <div className="form-group">
@@ -34,12 +80,15 @@ const LoginScreen = () => {
 
             <div className="col-md-6 login-form-2">
                 <h3>Registro</h3>
-                <form>
+                <form onSubmit={handleRegister}>
                     <div className="form-group">
                         <input
                             type="text"
                             className="form-control"
                             placeholder="Nombre"
+                            name='rName'
+                            value={rName}
+                            onChange={handleRegisterInputChange}
                         />
                     </div>
                     <div className="form-group">
@@ -47,13 +96,19 @@ const LoginScreen = () => {
                             type="email"
                             className="form-control"
                             placeholder="Correo"
+                            name="rEmail"
+                            value={rEmail}
+                            onChange={handleRegisterInputChange}
                         />
                     </div>
                     <div className="form-group">
                         <input
                             type="password"
                             className="form-control"
-                            placeholder="Contraseña" 
+                            placeholder="Contraseña"
+                            name="rPassword1" 
+                            value={rPassword1}
+                            onChange={handleRegisterInputChange}
                         />
                     </div>
 
@@ -61,7 +116,10 @@ const LoginScreen = () => {
                         <input
                             type="password"
                             className="form-control"
-                            placeholder="Repita la contraseña" 
+                            placeholder="Repita la contraseña"
+                            name="rPassword2"
+                            value={rPassword2}
+                            onChange={handleRegisterInputChange}
                         />
                     </div>
 
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,16 +1,45 @@
-import React from 'react'
-import { Route, BrowserRouter as Router , Switch, Redirect } from 'react-router-dom'
+import React, { useEffect }  from 'react'
+import { BrowserRouter as Router , Switch, Redirect } from 'react-router-dom'
 import LoginScreen from '../components/auth/LoginScreen'
 import CalendarScreen from '../components/calendar/CalendarScreen'
+import { useDispatch, useSelector } from 'react-redux';
+import { startChecking } from '../actions/auth';
+import { PublicRoute } from './PublicRoute';
+import { PrivateRoute } from './PrivateRoute';
+
+
 
 const AppRouter = () => {
+    const dispatch = useDispatch();
+    const { checking, uid } = useSelector( state => state.auth);
+
+    useEffect(() => {
+        
+        dispatch( startChecking() );
+
+    }, [dispatch])
+
+    if ( checking ) {
+        return (<h5>Espere...</h5>);
+    }
+
     return (
         <Router>
             <div>
                 <Switch>
-                     <Route exact path="/login" component={LoginScreen} />
-                      
-                    <Route exact path="/" component={CalendarScreen}/>
+                <PublicRoute 
+                exact 
+                path="/login" 
+                component={ LoginScreen }
+                isAuthenticated={ !!uid }
+                />
+
+                <PrivateRoute 
+                    exact 
+                    path="/" 
+                    component={ CalendarScreen } 
+                    isAuthenticated={ !!uid }
+                />
                     
                     <Redirect to='/' component={CalendarScreen} />
                 </Switch>
